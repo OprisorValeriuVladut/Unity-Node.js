4200ff941fbf1cda975f5d765cccc2ff19c29195
OprisorValeriuVladut <vlady,vlad95@gmail.com>
2018-08-28 20:05:35 +0300

    send first page

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + 'index.html');
+});
+
 app.listen(3000);
 console.log('You are listening to port 3000');
