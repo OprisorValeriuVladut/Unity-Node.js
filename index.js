3ef6c6e62391ad2b8026cb68ee7a7cc10172cf18
OprisorValeriuVladut <vlady,vlad95@gmail.com>
2018-08-28 19:38:26 +0300

    config serrver

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log('mergee!');
+var http = require('http');
 
-server.listen(process.env.PORT || 5000)
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(' mergeeeee !! ');
+})
+server.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
