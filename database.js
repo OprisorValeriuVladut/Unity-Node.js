477cac3b8bd79aaec710316aee810e436676f050
OprisorValeriuVladut <vlady,vlad95@gmail.com>
2018-08-30 22:08:54 +0300

    login system

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const connect = mongoose.connect(URL, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 const accountSchema = new mongoose.Schema({
-    first_name : String,
+    first_name: String,
     last_name: String,
     email: String,
     password: String
@@ -20,11 +20,39 @@ const saveAccounttoDB = function (first_name, last_name, email, password) {
         email: email,
         password: password
     }).save(function (err) {
-        if (err) return handleError(err);       
+        if (err) return handleError(err);
     });
 }
 
-module.exports.MongoClient = MongoClient;
+const getCollections = (email, password) => {
+    let canNavigate;
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(URL, { useNewUrlParser: true }, function (err, db) {
+            if (err) {
+                reject(err);
+            } else {
+                var dbo = db.db("vdstudio");
+                dbo.collection("registrations").find({}).toArray(function (err, result) {
+                    if (err) throw err;
+                    db.close();
+                    resolve(canNavigate = checkAvailability(email, password, result) ? true : false)
+                });
+            }
+        });
+    });
+};
+
+const checkAvailability = (email, password, result) => {
+    let canNavigate = false;
+    result.forEach(element => {
+        if (element['email'] === email && element['password'] === password) {
+            canNavigate = true;
+        }
+    });
+    return canNavigate;
+}
+
+module.exports.getCollections = getCollections;
 module.exports.URL = URL;
 module.exports.connect = connect;
 module.exports.accountSchema = accountSchema;
--- index.js
+++ index.js
@@ -7,25 +7,40 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.urlencoded({
     extended: true
- }));
- app.use(bodyParser.json());
+}));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/register', function(req, res){
-    res.sendFile(__dirname + '/register.html');
-});  
-
-app.post('/register-success', function(req, res){
-   db.saveAccounttoDB(req.body.first_name,req.body.last_name, req.body.email, req.body.password);
-   res.render('register-success');
+app.get('/home', function (req, res) {
+    res.render('index');
 });
 
-app.post('/login',function(req,res){
-    
- });
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+app.get('/dashboard', function (req, res) {
+    res.sendFile(__dirname + '/dashboard.html');
+});
+
+app.get('/register-success', function (req, res) {
+    res.render('register-success');
+});
+
+app.post('/register-success', function (req, res) {
+    db.saveAccounttoDB(req.body.first_name, req.body.last_name, req.body.email, req.body.password);
+    res.render('register-success');
+});
+
+app.post('/login', function (req, res) {
+    let redirectBool;
+    db.getCollections(req.body.email, req.body.password).then(function (result) {
+        res.render(result ? 'dashboard' : 'register');
+    });
+});
 
 
 app.listen(process.env.PORT || 5000)
